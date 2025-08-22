a27248351f14c4e31447584a2b5a7bb46d225171
Rodrigo Ortega <91147909+RodrigoOrtega1@users.noreply.github.com>
2025-08-22 11:36:45 -0600

    fix: cambio para que el back refleje el documento de diseno

--- app.py
+++ app.py
@@ -6,12 +6,20 @@ app = Flask(__name__)
 def agregar_resena():
     try:
         body = request.get_json()
-        if body:
-            return jsonify({"message": "Resena guardada", "Resena": body}), 201 #TODO: esto cambiara con la persistencia
+        if body and "materia" in body and "resena" in body:
+            subjects = body["materia"]
+            review = body["resena"]
+
+            if not subjects.strip():
+                return jsonify({"error": "El campo 'materia' no puede estar vacio"}), 400
+
+            if not review.strip():
+                return jsonify({"error": "El campo 'review' no puede estar vacio"}), 400
+            return jsonify({"message": "Resena guardada", "Resena": body}), 201  # TODO: Reemplazar con persistencia
         else:
-            return jsonify({"error": "Payload invalido"}), 400
+            return jsonify({"error": "Payload invalido. Se requieren los campos 'materia' y 'resena'"}), 400
     except Exception:
-        return jsonify({"error": "Payload invalido"}), 400
+        return jsonify({"error": "Error procesando el payload"}), 400
 
 if __name__ == "__main__":
     app.run(debug=True)
