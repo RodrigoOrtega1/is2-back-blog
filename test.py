697050118605b610a64825fa2d9e7e183d6ce830
Rodrigo Ortega <91147909+RodrigoOrtega1@users.noreply.github.com>
2025-08-22 11:37:27 -0600

    test: se agregaron pruebas unitarias

--- test.py
+++ test.py
@@ -8,8 +8,8 @@ class GuardarPruebas(unittest.TestCase):
 
     def test_post_valido(self):
         payload = {
-            "title": "Resena",
-            "content": "Gran clase"
+            "materia": "Matematicas",
+            "resena": "Gran clase"
         }
         response = self.app.post('/v0/elements', json=payload)
         self.assertEqual(response.status_code, 201)
@@ -17,11 +17,30 @@ class GuardarPruebas(unittest.TestCase):
         self.assertEqual(response_data["message"], "Resena guardada")
         self.assertEqual(response_data["Resena"], payload)
 
+    def test_post_invalido_faltan_campos(self):
+        payload = {
+            "resena" : "Gran clase"
+        }
+        response = self.app.post('v0/elements', json=payload)
+        self.assertEqual(response.status_code, 400)
+        response_data = response.get_json()
+        self.assertEqual(response_data["error"], "Payload invalido. Se requieren los campos 'materia' y 'resena'")
+
+    def test_post_invalido_campos_vacios(self):
+        payload = {
+            "materia" : "",
+            "resena" : ""
+        }
+        response = self.app.post('v0/elements', json=payload)
+        self.assertEqual(response.status_code, 400)
+        response_data = response.get_json()
+        self.assertIn("error", response_data)
+    
     def test_post_invalido(self):
         response = self.app.post('/v0/elements', headers={"Content-Type": "application/json"})
         self.assertEqual(response.status_code, 400)
         response_data = response.get_json()
-        self.assertEqual(response_data["error"], "Payload invalido")
+        self.assertEqual(response_data["error"], "Error procesando el payload")
 
 if __name__ == '__main__':
     unittest.main()
